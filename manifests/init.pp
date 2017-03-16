edf4c942f43db8c6ab1b0edce564ce7223d2df0f
gorantornqvist <gorantornqvist@users.noreply.github.com>
2017-03-16 15:22:01 +0100

    Added an additional parameter for passing a hiera array

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 class array_realize(
-  $realize = []
+  $realize = [],
+  $hiera_array = []
 ) {
   realize(any2array($realize))
+
+  $hiera_realize = hiera_array('array_realize::hiera_array', $hiera_array)
+  realize($hiera_realize)
 }
